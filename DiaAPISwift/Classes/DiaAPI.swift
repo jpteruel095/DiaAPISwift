dcacd6b8f5fe7b69d17290cdbffd70cecf99c709
John Patrick Teruel <jpteruel95@gmail.com>
2020-03-10 00:03:05 +0800

    Implementing requests

--- DiaAPISwift/Classes/DiaAPI.swift
+++ DiaAPISwift/Classes/DiaAPI.swift
@@ -5,5 +5,86 @@
 //  Created by John Patrick Teruel on 3/9/20.
 //
 
-import Foundation
+import Alamofire
+import SwiftyJSON
 
+public protocol RequestProtocol{
+    func getParameters() -> Parameters
+}
+
+public protocol EndpointProtocol{
+    ///Request with Alamofire Parameters, Progress Callback, Completion Callback, and flags to Log request and result
+    func request(parameters: Parameters?,
+                 progressCallback:((Progress) -> Void)?,
+                 completion:((JSON?, Error?) -> Void)?,
+                 shouldLog: Bool,
+                 shouldLogResult: Bool)
+    
+    ///Request with Request Protocol, Progress Callback, Completion Callback, and flags to Log request and result
+    func request(request: RequestProtocol?,
+                 progressCallback:((Progress) -> Void)?,
+                 completion:((JSON?, Error?) -> Void)?,
+                 shouldLog: Bool,
+                 shouldLogResult: Bool)
+    
+    ///Request with Request Protocol and with progress callback, logged by default
+    func request(request: RequestProtocol?,
+                 progressCallback:((Progress) -> Void)?,
+                 completion:((JSON?, Error?) -> Void)?)
+    
+    ///Request with Request Protocol and without progress callback, logged by default
+    func request(request: RequestProtocol?,
+                 completion:((JSON?, Error?) -> Void)?)
+    
+    ///Request with completion callback only, logged by default
+    func request(completion:((JSON?, Error?) -> Void)?)
+}
+
+public extension EndpointProtocol{
+    func request(parameters: Parameters? = nil,
+                 progressCallback:((Progress) -> Void)? = nil,
+                 completion:((JSON?, Error?) -> Void)? = nil,
+                 shouldLog: Bool = true,
+                 shouldLogResult: Bool = true){
+        
+    }
+    
+    func request(request: RequestProtocol?,
+                 progressCallback:((Progress) -> Void)?,
+                 completion:((JSON?, Error?) -> Void)?,
+                 shouldLog: Bool,
+                 shouldLogResult: Bool){
+        self.request(parameters: request?.getParameters(),
+                     progressCallback: progressCallback,
+                     completion: completion,
+                     shouldLog: shouldLog,
+                     shouldLogResult: shouldLogResult)
+    }
+    
+    func request(request: RequestProtocol?,
+                 progressCallback:((Progress) -> Void)?,
+                 completion:((JSON?, Error?) -> Void)?){
+        self.request(parameters: request?.getParameters(),
+                     progressCallback: progressCallback,
+                     completion: completion,
+                     shouldLog: true,
+                     shouldLogResult: true)
+    }
+    
+    func request(request: RequestProtocol?,
+                 completion:((JSON?, Error?) -> Void)?){
+        self.request(parameters: request?.getParameters(),
+                     progressCallback: nil,
+                     completion: completion,
+                     shouldLog: true,
+                     shouldLogResult: true)
+    }
+    
+    func request(completion:((JSON?, Error?) -> Void)?){
+        self.request(parameters: nil,
+                     progressCallback: nil,
+                     completion: completion,
+                     shouldLog: true,
+                     shouldLogResult: true)
+    }
+}
